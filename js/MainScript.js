36a111c7788a7f12eee348c256a6f52871e41b09
Martin Høite <martinhblizz@gmail.com>
2019-05-20 13:08:17 +0200

    Opsat forside og overordnet layout, mangler responsive og content

---
+++ js/MainScript.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.carousel').carousel();
+    $('.slider').slider();
+    $('.materialboxed').materialbox();
+});
+
+// $('.carousel.carousel-slider').carousel({
+//     fullWidth: true,
+//     indicators: true
+// });
+
