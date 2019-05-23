61730fc5327a17052d6f02f9f29605b23d56cb75
Martin Høite <mh@komunikado.dk>
2019-05-23 11:54:12 +0200

    Prepared products for insertion

---
+++ js/Products.js
@@ -0,0 +1,29 @@
+
+
+function Product(Name, Description, Price, Type, Imgpath) {
+    this.Name = Name;
+    this.Description = Description;
+    this.Price = Price;
+    this.Type = Type;
+    this.Imgpath = Imgpath;
+}
+
+var products = [
+
+    new Product(
+        "Tørklæde silke batik (Bangladesh", //Name
+        "Tørklæde, silke, batik, Bangladesh",
+        149,
+        "Tørklæde",
+        "images/products/torklade/torklade-silke-batik_bangladesh.jpg"
+    ),
+    new Product(
+        
+    ),
+
+]
+
+// Navn: Tørklæde silke batik (Bangladesh)
+// Beskrivelse: Tørklæde, silke, batik, Bangladesh
+// Billedenavn: torklade-silke-batik_bangladesh
+// Pris: 149 kr.
