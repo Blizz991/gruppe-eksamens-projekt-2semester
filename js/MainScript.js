4cf17dee023220f8292c9fbd09360435d6380a00
Martin Høite <eaamhoe@students.eaaa.dk>
2019-05-27 12:35:33 +0200

    Updated menu and added functionality to highlight active page in menu

--- js/MainScript.js
+++ js/MainScript.js
@@ -5,6 +5,40 @@ $(document).ready(function () {
     $('.tooltipped').tooltip();
 });
 
+//From: https://makitweb.com/highlight-current-page-menu-item-with-jquery/
+// Get current page URL
+var url = window.location.href;
+
+// remove # from URL
+url = url.substring(0, (url.indexOf("#") == -1) ? url.length : url.indexOf("#"));
+
+// remove parameters from URL
+url = url.substring(0, (url.indexOf("?") == -1) ? url.length : url.indexOf("?"));
+
+// select file name
+url = url.substr(url.lastIndexOf("/") + 1);
+
+// If file name not avilable
+if (url == '') {
+    url = 'index.html';
+}
+console.log(url);
+
+// Loop all menu items
+$('.menu li').each(function () {
+
+    // select href
+    var href = $(this).find('a').attr('href');
+    console.log(href);
+
+    // Check filename
+    if (url == href) {
+
+        // Add active class
+        $(this).addClass('active');
+    }
+});
+
 // $('.carousel.carousel-slider').carousel({
 //     fullWidth: true,
 //     indicators: true
